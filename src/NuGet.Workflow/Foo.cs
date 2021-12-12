b3a5b3f36dc2c67a7c6e846c1b6f21f16cea328c
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2021-12-12 17:51:55 -0500

    New Package (#10)

--- src/NuGet.Workflow/Foo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Itabaiyu.NuGet.Workflow
-{
-    // mock code to package up.
-    public class Foo
-    {
-        public string Bar() => "Bar";
-    }
-}
---
+++ tests/NuGet.Workflow.UnitTests/FibonacciTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Linq;
+
+namespace NuGet.Workflow.UnitTests
+{
+    /// <summary>
+    ///     It is a good practice to have test coverage for your NuGet package. A widely used code library should generally
+    ///     have very high test coverage, preferably 100% if you've added meaningful coverage exclusions properly.
+    /// </summary>
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [Test]
+        public void Get_generates_expected_fibonacci_numbers()
+        {
+            // arrange
+            const int takeCount = 10000;
+            var fibonacci = new Fibonacci();
+
+            // act
+            var fibonacciNumbers = fibonacci.Take(takeCount).ToList();
+
+            // assert
+            fibonacciNumbers.Count.Should().Be(takeCount);
+            fibonacciNumbers.Should().BeInAscendingOrder();
+
+            fibonacciNumbers[0].Should().Be(0);
+            fibonacciNumbers[1].Should().Be(1);
+
+            for (var index = 2; index < fibonacciNumbers.Count; ++index)
+            {
+                fibonacciNumbers[index].Should().Be(fibonacciNumbers[index - 1] + fibonacciNumbers[index - 2]);
+            }
+        }
+    }
+}
